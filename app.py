81cf12e78950f2b5f7945ffa3ebfa1ba5d15b5e7
SpencerRenfro <spencer.renfro13@gmail.com>
2026-01-29 11:36:25 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+"""
+Flask Portfolio App
+Author: Spencer Renfro
+Date: 2026-1-29
+"""
+from flask import Flask, render_template
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    now = datetime.now()
+    return render_template("index.html", now=now)
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name='world'):
+    return render_template('hello.html', name=name)
+
+@app.route("/polytopia")
+def polytopia():
+    return render_template('polytopia.html')
+
+@app.route("/steamaction")
+def steamaction():
+    return render_template('steamaction.html')
+
+
+
