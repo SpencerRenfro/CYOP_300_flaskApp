025bef239e289c4a49be55b6be2aad8e8e7a9a8e
SpencerRenfro <spencer.renfro13@gmail.com>
2026-01-30 14:35:03 -0700

    adds user sign up

--- app.py
+++ app.py
@@ -3,10 +3,36 @@ Flask Portfolio App
 Author: Spencer Renfro
 Date: 2026-1-29
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from datetime import datetime
-app = Flask(__name__)
+import json
 
+
+app = Flask(__name__)
+USERS_FILE = "static/data/users.json"
+
+# Helper functions
+
+    # Load users from JSON file
+def load_users():
+    with open(USERS_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+    
+    # Save users to JSON file
+def save_users(data):
+    with open(USERS_FILE, "w", encoding="utf-8") as f:
+        json.dump(data,f, indent=4)
+        
+    # Check if username exists
+
+def check_username_exists(username, email):
+    users = load_users()
+    for user in users["users"]:
+        if user["username"].lower() == username.lower() or user["email"].lower() == username.lower():
+            return True
+    return False
+
+# Routes
 @app.route("/")
 def index():
     now = datetime.now()
@@ -29,12 +55,49 @@ def steamaction():
 def inventory_management():
     return render_template('inventory_management.html')
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template('signup.html')
+    
+    if request.method =="GET":
+        return render_template('signup.html')
+    
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    if not username or not email or not password:
+        return render_template('signup.html', error="Please fill out all fields")
+    
+    data = load_users()
+    
+    # Check for existing username
+    if check_username_exists(username, email):
+        return render_template('signup.html', error="Username or email already registered")
+    
+    # Create new user
+    data["users"].append({
+        "username" : username,
+        "email" : email,
+        "password" : password
+    })
+    save_users(data)
+    print("User created:", username, email, password)
+    
+    return redirect(url_for('index'))
 
-@app.route("/login")
+@app.route("/login", methods=["GET"])
 def login():
-    return render_template('login.html')
+    username = request.args.get("username")
+    password = request.args.get("password")
+    
+    print("Hello from login")
 
+    if username and password:
+        print("LOGIN GET:", username, password)
 
+    return render_template("login.html")
+
+        
+print("Hello from app.py")
+if __name__ == "__main__":
+    app.run(debug=True)
